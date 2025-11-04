9e8a94c87a182173b868dee32f5c73f3576cef07
Aya <163719405+sapimouuu@users.noreply.github.com>
2025-11-04 20:46:07 +0800

    PRAK404-2510817220023-NURFITRIA RAMADHANI.py

--- PRAK404-2510817220023-NURFITRIA RAMADHANI.py
+++ PRAK404-2510817220023-NURFITRIA RAMADHANI.py
@@ -18,8 +18,11 @@ while True:
     elif pil==4:
         nil1=float(input("Masukkan nilai pertama : "))
         nil2=float(input("Masukkan nilai kedua : "))
-        hasil = nil1/nil2
-        print(f"Hasil pembagian antara {nil1:.2f} dengan {nil2:.2f} adalah {hasil:.2f}\n")
+        if nil2==0:
+            print("Tak tentu(indeterminate)\n")
+        else:    
+            hasil = nil1/nil2
+            print(f"Hasil pembagian antara {nil1:.2f} dengan {nil2:.2f} adalah {hasil:.2f}\n")
     elif pil==5:
         print("Terimakasih telah menggunakan Kalkulator Nurfitria Ramadhani")
         break
